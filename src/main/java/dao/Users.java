2682e275ccb3b8af813faaba58a9237cc008b867
Wilmarie De La Cruz <wilmarie.delacruz@gmail.com>
2023-02-10 09:53:38 -0600

    adding dao folder

---
+++ src/main/java/dao/Users.java
@@ -0,0 +1,10 @@
+package dao;
+
+import com.codeup.adlister.models.User;
+
+import java.util.List;
+
+public interface Users {
+    User findByUsername(String username);
+    Long insert(User user);
+}
